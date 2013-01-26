cda361fbd824411aa586bae000aeef0f4af6b385
AricwithanA <mraricwithana@hotmail.com>
2013-01-25 20:40:35 -0600

    Removed old code, added more system events to the control list.

--- SDCARD/DOMLauncher/default/js/callbacks.js
+++ SDCARD/DOMLauncher/default/js/callbacks.js
@@ -37,14 +37,16 @@ function applistCallback(appList){
 
 //Clock Timer 
 clockTimer = null;
-	
+	$clockSec = $("#sec")
+	$clockHour = $("#hour")
+	$clockMin = $("#min")
  function clock() {
 	date = new Date();
 	mins = date.getMinutes();
 
-	$("#sec").css({"-webkit-transform" : "rotate(" + date.getSeconds() * 6 + "deg)"});
-	$("#hour").css({"-webkit-transform" : "rotate(" + date.getHours() * 30 + (mins / 2) + "deg)"});
-	$("#min").css({"-webkit-transform": "rotate(" + mins * 6 + "deg)"});
+	$clockSec.css({"-webkit-transform" : "rotate(" + date.getSeconds() * 6 + "deg)"});
+	$clockHour.css({"-webkit-transform" : "rotate(" + date.getHours() * 30 + (mins / 2) + "deg)"});
+	$clockMin.css({"-webkit-transform": "rotate(" + mins * 6 + "deg)"});
 	
 	 date = null;
 	 mins = null;
@@ -183,6 +185,8 @@ function toggledataCallback(args){
 	// 		on desired users wants) to the function that changes the UI.
 	//		This was done to simply the code, not a neccessary thing.
 		// @ object_datawifiSignal 
+	$meterwifiBar = $('#meter_Wifi').children('div');
+	$meterwifiLabel = $('#meter_Wifi').siblings('div');
 function wifisignalCallback(strengthDbm){
 	
 	/*Begin Theme Specific Editible Code*/
@@ -207,20 +211,75 @@ function wifisignalCallback(strengthDbm){
 		} else if (strengthDbm > -59.5 && strengthDbm <= -57.25) { wifiChange(90);
 		} else if (strengthDbm > -57.25 && strengthDbm <= -55) { wifiChange(95);
 		} else if (strengthDbm > -55) { wifiChange(100);
-		}
+		}	
+	/*End Theme Specific Editible Code*/
+}
+
+	function wifiChange(percentage){
+		$meterwifiLabel.text('Wifi Signal: ' + percentage + '%');
+		$meterwifiBar.css('width', percentage + '%');
+	}
+
+
+
+//Cellular Signal Callback
+	// @ strengthDbm - returns a negative integer, -dBm
+	// Since people in the past have bickered about the correctness
+	// 		of the displayed signal value, any user can decide for 
+	//		themselves to remove any chance of complaint.
+	// @ cellsignalChange() - Sends the converted number (conversion is based 
+	// 		on desired users wants) to the function that changes the UI.
+	//		This was done to simply the code, not a neccessary thing.
+	//
+	//	This function is called directly from within the Cordova API Java Plugin.
+	//		DO NOT CHANGE THE FUNCTION CALL BACK NAMESPACE!
+		// @ object_datawifiSignal 
+	$metercellularBar = $('#meter_Cellular').children('div');
+	$metercellularLabel = $('#meter_Cellular').siblings('div');
 	
-		function wifiChange(percentage){
-			$('#test').text(percentage);
+function cellsignalCallback(strengthDbm){
+	/*Begin Theme Specific Editible Code*/
+		if (strengthDbm <= -97.75) { cellsignalChange(0); 
+		} else if (strengthDbm > -97.75 && strengthDbm <= -95.5) { cellsignalChange(10);  
+		} else if (strengthDbm > -95.5 && strengthDbm <= -93.25) { cellsignalChange(20);
+		} else if (strengthDbm > -93.25 && strengthDbm <= -91) { cellsignalChange(30);
+		} else if (strengthDbm > -91 && strengthDbm <= -88.75) { cellsignalChange(40);
+		} else if (strengthDbm > -88.75 && strengthDbm <= -86.5) { cellsignalChange(50);
+		} else if (strengthDbm > -86.5 && strengthDbm <= -84.25) { cellsignalChange(60);
+		} else if (strengthDbm > -84.25 && strengthDbm <= -82) { cellsignalChange(70);
+		} else if (strengthDbm > -82 && strengthDbm <= -79.75) { cellsignalChange(80);
+		} else if (strengthDbm > -79.75 && strengthDbm <= -77.5) { cellsignalChange(90);
+		} else if (strengthDbm > -77.5 && strengthDbm <= -75.25) { cellsignalChange(100);
 		}
 	/*End Theme Specific Editible Code*/
 }
 
+	function cellsignalChange(percentage){
+		$metercellularLabel.text('Cellular Signal: ' + percentage + '%');
+		$metercellularBar.css('width', percentage + '%');
+	}
 
 
 
-
-
-
+//Battery Level Receiver 
+	// @.isPlugged - boolean true/false if the device is plugged in.
+	// @ .level - integer 0-100, battery level value
+	// @ info  - MUST be used as the callback Variable!  This is a Cordova requirement, not mine.
+		// @ object_toggle34G 
+	$meterbatteryBar = $('#meter_Battery').children('div');
+	$meterbatteryLabel = $('#meter_Battery').siblings('div');
+	
+function batterylevelCallback(info){ 
+	/*Begin Theme Specific Editible Code*/
+		if(info.isPlugged != false){
+			$meterbatteryLabel.text('Battery Charging:  '+info.level+'%');  
+			$meterbatteryBar.css('width', info.level + '%');
+		}else{
+			$meterbatteryLabel.text('Battery Level:  '+info.level+'%'); 
+			$meterbatteryBar.css('width', info.level + '%');
+		}
+	/*End Theme Specific Editible Code*/
+}
 
 
 
