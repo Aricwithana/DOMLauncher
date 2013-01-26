f758f2d96ad9c81fbcd89486b5417c830ec2d951
AricwithanA <mraricwithana@hotmail.com>
2013-01-25 22:13:41 -0600

    Commenting code

--- SDCARD/DOMLauncher/default/js/callbacks.js
+++ SDCARD/DOMLauncher/default/js/callbacks.js
@@ -1,13 +1,12 @@
-function applistCallback(appList){
-	//Parse the appList
-		
+function applistCallback(appList){	
+		//Parse the appList
 		appListArray = JSON.parse(appList);
 		//For each item in the JSON Array
-			// @ appName - Application Name ('DOMLauncher')
-			// @ appLaunch - Package class name ('com.awaa.domlauncher')
-			// @ appActivity - Acivity Intent ('.DialtactsContactsEntryActivity')
-		$appPanel = $('.appPanel_Content');
-		$appPanel.empty();
+			//@ appName - Application Name ('DOMLauncher')
+			//@ appLaunch - Package class name ('com.awaa.domlauncher')
+			//@ appActivity - Acivity Intent ('.DialtactsContactsEntryActivity')
+			$appPanel = $('.appPanel_Content');
+			$appPanel.empty();
 		$.each(appListArray, function(e) {
 			 appName = this.name
 			 appLaunch = this.package
@@ -33,23 +32,26 @@ function applistCallback(appList){
 	http://css-tricks.com/css3-clock/
 	PUBLISHED NOVEMBER 17, 2008
 	Toby Pitman http://www.tobypitman.com/
+	Modified by Aricwithana
 */
 
-//Clock Timer 
-clockTimer = null;
-	$clockSec = $("#sec")
-	$clockHour = $("#hour")
-	$clockMin = $("#min")
- function clock() {
+//@ clockTimer - Nulls the setInterval object.
+//@ clockSec, clockHour, clockMin - Caches repeatedly Called DOM element.
+	clockTimer = null;
+	$clockSec = $("#sec");
+	$clockHour = $("#hour");
+	$clockMin = $("#min");
+	
+function clock() {
 	date = new Date();
 	mins = date.getMinutes();
-
+	
 	$clockSec.css({"-webkit-transform" : "rotate(" + date.getSeconds() * 6 + "deg)"});
 	$clockHour.css({"-webkit-transform" : "rotate(" + date.getHours() * 30 + (mins / 2) + "deg)"});
 	$clockMin.css({"-webkit-transform": "rotate(" + mins * 6 + "deg)"});
 	
-	 date = null;
-	 mins = null;
+	date = null;
+	mins = null;
 }
 
 
@@ -57,30 +59,39 @@ clockTimer = null;
 
 
 
-
+/*Switches Widget Plugin
+*	This plugin creates native-like switches.  Each element with the secified class
+*		and correct DOM structure will create a working switch.  There is no 'universal'
+*		Widget solution for every theme.  It should be customized.
+*/
 function switches(){
 	$( ".switch>div:first-child" ).each(function(){
 		$(this).draggable({containment: "parent",distance:0,
 			create: function( event, ui ) {},
 			stop: function( event, ui ) {
-				handle = $(this);
+				handle = $(this); //Switch Handle
 				switchID = $(handle).parent('.switch').attr('id');
 				handlePOS =  parseInt($(handle).css('left'))
 				
+				//Controls the switch state response on touch end.  Animates and called the switchCallback function passing its state and switch ID.
 				if(handlePOS >= 49){if(handlePOS != 100){$(handle).animate({left:'100px'}, 250).addClass('on');}  switchCallback({state:"on", id:switchID});}
 				if(handlePOS <= 50){$(handle).animate({left:'0px'}, 250).removeClass('on'); switchCallback({state:"off", id:switchID});}	  
+				
 				handlePOS = null;
 				switchID = null;
 				handle = null;
 				
 			},
 			drag: function( event, ui ) {  
-				handle = $(this);
+				handle = $(this); //Switch Handle
 				handlePOS = parseInt($(handle).css('left'));
 				
+				//Controls the switch state response while dragging.  
 				if( handlePOS >= 49  && $(handle).hasClass('on') == false){$(handle).addClass('on'); }
 				if(handlePOS <= 50 && $(handle).hasClass('on') == true){$(handle).removeClass('on');}  
+				//Prevents the screen scroll while interacting with a switch.
 				$('#slider>ul, #slider, #slider>ul>li').on('touchmove', function(e){e.stopPropagation();});   
+				
 				handlePOS = null;
 				handle = null;
 			}
@@ -88,40 +99,60 @@ function switches(){
 	});
 }
 
+//Switch Widget Callback
+	//@ switchID - ID of the switch
+	//@ state - Current end state of the switch
 function switchCallback(args){
 	switchID = args.id || null;
 	state = args.state || null;
-
-	if(switchID === "toggle_fullScreen"){
-		toggleStatusbar({});
-	}
 	
-	if(switchID === "toggle_wifi" && state === "on"){
-		toggleWifi({state:"on"});
-	}
-	
-	if(switchID === "toggle_wifi" && state === "off"){
-		toggleWifi({state:"off"});
-	}
-	
-	if(switchID === "toggle_data" && state === "off"){
-		toggleData({state:"off"});
-	}
-	if(switchID === "toggle_data" && state === "on"){
-		toggleData({state:"on"});
-	}
-
+	/*Begin Theme Specific Editible Code*/
+		if(switchID === "toggle_fullScreen"){ 
+			//Toggle Full Screen Call Function
+				// @ toggleStatusbar({}) - Just call it, the app restarts with full screen toggled on or off.
+				//Also accepts {check:"true"} to just check the if the app is fullscreen or not.
+			toggleStatusbar({}); 
+		}
+		
+		//Toggle Wifi Call Function
+			// @ toggleWifi({}) - Call Function
+			// @ {check:"on/off"} - Sets the state of the Wifi Connection.
+				//Also accepts {check:"true"} to just check the current wifi connection state.
+				//Also accepts {state:"toggle"} to just toggle the connection.
+			//The plugin automatically creates a listener for the signal value which requires a callback function provided below.
+		if(switchID === "toggle_wifi" && state === "on"){
+			toggleWifi({state:"on"});
+		}
+		if(switchID === "toggle_wifi" && state === "off"){
+			toggleWifi({state:"off"});
+		}
+		
+		//Toggle Data Call Function
+			// @ toggleData({}) - Call Function
+			// @ state - Sets the state of the Data Connection.
+				//Also accepts {check:"true"} to just check the current Data connection state.
+				//Also accepts {state:"toggle"} to just toggle the connection.
+		if(switchID === "toggle_data" && state === "off"){
+			toggleData({state:"off"});
+		}
+		if(switchID === "toggle_data" && state === "on"){
+			toggleData({state:"on"});
+		}
+	/*End Theme Specific Editible Code*/
 }	
 	
+
+//Fullscreen Toggle Callback
+	// @ returnVal - returns boolean true/false depending on if the app is full screen or not.
 function fullscreentoggleCallback(args){
 	returnVal = args.returnVal
-	
-	if(returnVal != true){
-		$('#toggle_fullScreen>div:first-child').css('left', '0px').removeClass('on');
-	}else{
-		$('#toggle_fullScreen>div:first-child').css('left', '100px').addClass('on');
-	}
-	
+	/*Begin Theme Specific Editible Code*/
+		if(returnVal != true){
+			$('#toggle_fullScreen>div:first-child').css('left', '0px').removeClass('on');
+		}else{
+			$('#toggle_fullScreen>div:first-child').css('left', '100px').addClass('on');
+		}
+	/*End Theme Specific Editible Code*/
 }
 
 
@@ -129,12 +160,12 @@ function fullscreentoggleCallback(args){
 //Wifi Toggle Callback
 	// @ check - returns the boolean true/false if the function is just being used to check information.
 	// @ returnVal - returns boolean true/false depending on if the data connection is on or off.
-		// @ object_toggleWifi 
-	
+	// @ state - returns the state on/off/toggle that was supplied in the original call.
 function togglewifiCallback(args){
 	 check = args.check
 	 returnVal = args.returnVal
 	 state = args.state
+	
 	/*Begin Theme Specific Editible Code*/
 		if(returnVal === true && $('#toggle_wifi>div:first-child').hasClass('on') === false){
 			$('#toggle_wifi>div:first-child').animate({left: '100px'}, 250).addClass('on');
@@ -142,6 +173,7 @@ function togglewifiCallback(args){
 		if(returnVal === false && $('#toggle_wifi>div:first-child').hasClass('on') === true){
 			$('#toggle_wifi>div:first-child').animate({left: '0px'}, 250).removeClass('on');
 		}
+		//Until there is a 'state listener' for when connections turn on and off, this timeout is needed to correctly respond to if the data connecton is active or not.
 		setTimeout(function(){toggleData({check:"true"});}, 10000);
 	/*End Theme Specific Editible Code*/
 }
@@ -159,12 +191,9 @@ function toggledataCallback(args){
 	/*Begin Theme Specific Editible Code*/
 		if(returnVal === true && $('#toggle_data>div:first-child').hasClass('on') === false){
 			$('#toggle_data>div:first-child').animate({left: '100px'}, 250).addClass('on');
-			
 		}
 		if(returnVal === false && $('#toggle_data>div:first-child').hasClass('on') === true){
 			$('#toggle_data>div:first-child').animate({left: '0px'}, 250).removeClass('on');
-			
-		
 		}
 	/*End Theme Specific Editible Code*/
 }
@@ -176,7 +205,7 @@ function toggledataCallback(args){
 
 
 
-//Wifi Signal Callback
+//Wifi Signal Callback - A REQUIRED FUNCTION IF WIFI CONTROLS PLUGIN IS USED.
 	// @ returnVal - returns a negative integer, -dBm
 	// Since people in the past have bickered about the correctness
 	// 		of the displayed signal value, any user can decide for 
@@ -184,9 +213,12 @@ function toggledataCallback(args){
 	// @ wifiChange() - Sends the converted number (conversion is based 
 	// 		on desired users wants) to the function that changes the UI.
 	//		This was done to simply the code, not a neccessary thing.
-		// @ object_datawifiSignal 
-	$meterwifiBar = $('#meter_Wifi').children('div');
-	$meterwifiLabel = $('#meter_Wifi').siblings('div');
+	//	This function is called directly from within the Cordova API Java Plugin.
+	//		DO NOT CHANGE THE FUNCTION CALL BACK NAMESPACE!
+	// @ meterwifiBar - Caches repeatedly Called DOM element.
+	// @ meterwifiLabel - Caches repeatedly Called DOM element.
+		$meterwifiBar = $('#meter_Wifi').children('div');
+		$meterwifiLabel = $('#meter_Wifi').siblings('div');
 function wifisignalCallback(strengthDbm){
 	
 	/*Begin Theme Specific Editible Code*/
@@ -214,7 +246,7 @@ function wifisignalCallback(strengthDbm){
 		}	
 	/*End Theme Specific Editible Code*/
 }
-
+	//Used with the Wifi Signal Callback Function.
 	function wifiChange(percentage){
 		$meterwifiLabel.text('Wifi Signal: ' + percentage + '%');
 		$meterwifiBar.css('width', percentage + '%');
@@ -253,7 +285,7 @@ function cellsignalCallback(strengthDbm){
 		}
 	/*End Theme Specific Editible Code*/
 }
-
+	//Used with the Cellular Signal Callback Function.
 	function cellsignalChange(percentage){
 		$metercellularLabel.text('Cellular Signal: ' + percentage + '%');
 		$metercellularBar.css('width', percentage + '%');
@@ -265,7 +297,6 @@ function cellsignalCallback(strengthDbm){
 	// @.isPlugged - boolean true/false if the device is plugged in.
 	// @ .level - integer 0-100, battery level value
 	// @ info  - MUST be used as the callback Variable!  This is a Cordova requirement, not mine.
-		// @ object_toggle34G 
 	$meterbatteryBar = $('#meter_Battery').children('div');
 	$meterbatteryLabel = $('#meter_Battery').siblings('div');
 	
