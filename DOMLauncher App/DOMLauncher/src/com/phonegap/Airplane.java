3dde8ee944ab7104f278042dba8dc788c4753ca2
AricwithanA <mraricwithana@hotmail.com>
2013-03-09 17:09:08 -0600

    Removed the two seperate function calls in the java side. No longer necessary.

--- DOMLauncher App/DOMLauncher/src/com/phonegap/Airplane.java
+++ DOMLauncher App/DOMLauncher/src/com/phonegap/Airplane.java
@@ -39,37 +39,23 @@ public class Airplane extends CordovaPlugin {
 			}
 			
 			if(action.equals("toggle")){
-				toggleAirplane(); 
-				boolean isEnabled = this.isEnabled();
+				// read the airplane mode setting
+				boolean isEnabled = Settings.System.getInt(this.cordova.getActivity().getContentResolver(), Settings.System.AIRPLANE_MODE_ON, 0) == 1;
+			
+				// toggle airplane mode
+				Settings.System.putInt(this.cordova.getActivity().getContentResolver(), Settings.System.AIRPLANE_MODE_ON, isEnabled ? 0 : 1);
+			
+				// Post an intent to reload
+				Intent intent = new Intent(Intent.ACTION_AIRPLANE_MODE_CHANGED);
+				intent.putExtra("state", !isEnabled);
+				this.cordova.getActivity().sendBroadcast(intent);
 				
-				callbackContext.success(new JSONObject().put("returnVal", isEnabled));
+				boolean endisEnabled = Settings.System.getInt(this.cordova.getActivity().getContentResolver(), Settings.System.AIRPLANE_MODE_ON, 0) == 1;
+				
+				callbackContext.success(new JSONObject().put("returnVal", endisEnabled));
 			}
 
 		return true;
-	}
-	
-	
-	private void toggleAirplane() {	
-	
-		// read the airplane mode setting
-		boolean isEnabled = Settings.System.getInt(this.cordova.getActivity().getContentResolver(), Settings.System.AIRPLANE_MODE_ON, 0) == 1;
-	
-		// toggle airplane mode
-		Settings.System.putInt(this.cordova.getActivity().getContentResolver(), Settings.System.AIRPLANE_MODE_ON, isEnabled ? 0 : 1);
-	
-		// Post an intent to reload
-		Intent intent = new Intent(Intent.ACTION_AIRPLANE_MODE_CHANGED);
-		intent.putExtra("state", !isEnabled);
-		this.cordova.getActivity().sendBroadcast(intent);	
-	}
-
-	private boolean isEnabled() {
-      
-	     boolean isEnabled = Settings.System.getInt(
-				this.cordova.getActivity().getContentResolver(), 
-			      Settings.System.AIRPLANE_MODE_ON, 0) == 1;
-	    
-	    return isEnabled;
 	}
 
 }
